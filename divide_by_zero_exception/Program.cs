be652ecb8328e9f0909060dfb843b49d5b67b8dd
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-03-18 20:56:21 +1100

    Initial commit

---
+++ divide_by_zero_exception/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * C# Program to Demonstrate DivideByZero Exception
+ */
+
+using System;
+
+namespace divide_by_zero_exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int result = 15 / int.Parse("0");
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.Write(e.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
